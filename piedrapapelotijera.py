f48daf81dc3d318858406c630eb7c4acbb77d895
= <=>
2024-04-10 12:45:46 +0200

    Segunda versión - decimos lo que saca la máquina

--- piedrapapelotijera.py
+++ piedrapapelotijera.py
@@ -6,5 +6,9 @@ print ("1 = piedra")
 tirada = input("Escoge tu tirada: ")
 tiradaordenador = random.randint(1,3)
 
-print (tirada)
-print (tiradaordenador)
+if tiradaordenador == 1:
+    print("el ordenador ha sacado piedra")
+elif tiradaordenador == 2:
+    print("el ordenador ha sacado papel")
+elif tiradaordenador == 3:
+    print("el ordenador ha sacado tijera")
