bb4293363d282a562871cb2b53a6207371adf144
= <=>
2024-04-13 11:49:05 +0200

    Comprobación del ganador

--- piedrapapelotijera.py
+++ piedrapapelotijera.py
@@ -5,7 +5,7 @@ print("Piedra, Papel o tijera, v0.3")
 print ("1 = piedra")
 print ("1 = piedra")
 print ("1 = piedra")
-tirada = input("Escoge tu tirada: ")
+tirada = int (input("Escoge tu tirada: "))
 tiradaordenador = random.randint(1,3)
 
 if tiradaordenador == 1:
@@ -14,3 +14,21 @@ elif tiradaordenador == 2:
     print("el ordenador ha sacado papel")
 elif tiradaordenador == 3:
     print("el ordenador ha sacado tijera")
+if tirada == 1 and tiradaordenador == 1:
+    print ("empate")
+elif tirada == 1 and tiradaordenador == 2:
+    print ("maquina gana")
+elif tirada == 1 and tiradaordenador == 3:
+    print ("jugador gana")
+elif tirada == 2 and tiradaordenador == 1:
+    print ("jugador gana")
+elif tirada == 2 and tiradaordenador == 2:
+    print ("empate")
+elif tirada == 2 and tiradaordenador == 3:
+    print ("tijeras")
+elif tirada == 3 and tiradaordenador == 1:
+    print ("maquina gana")
+elif tirada == 1 and tiradaordenador == 2:
+    print ("jugador gana")
+elif tirada == 1 and tiradaordenador == 3:
+    print ("empate")
